2fc2e77e0f8ef4ef14703c5eb492ae64252aabd8
andrew brown <dr.andrew.david.brown@gmail.com>
2023-11-12 16:02:49 +0000

    add model schemas

---
+++ resource_keeper/models.py
@@ -0,0 +1,28 @@
+from resource_keeper import db
+
+
+class Category(db.Model):
+    # schema for the Category model
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(25), unique=True, nullable=False)
+    tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return self.category_name
+
+
+class Resource(db.Model):
+    # schema for the Resource model
+    id = db.Column(db.Integer, primary_key=True)
+    resource_name = db.Column(db.String(50), unique=True, nullable=False)
+    resource_description = db.Column(db.Text, nullable=False)
+    resource_accessed = db.Column(db.Boolean, default=False, nullable=False)
+    date_created = db.Column(db.Date, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return "#{0} - Task: {1} | Urgent: {2}".format(
+            self.id, self.task_name, self.is_urgent
+        )
