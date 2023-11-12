57c87b501c020076d7cdb90140e9a5bb315b6452
andrew brown <dr.andrew.david.brown@gmail.com>
2023-11-12 19:47:39 +0000

    add routes for home edit category and category

--- resource_keeper/routes.py
+++ resource_keeper/routes.py
@@ -4,5 +4,28 @@ from resource_keeper.models import Category, Resource
 
 @app.route("/")
 def home():
-    return render_template("categories.html")
+    # convert cursor object returned by query into python list
+    # NB each element of list represents a row of fields of model
+    # row fields are accessed via dot notation
+    categories = list(Category.query.all())
 
+    # categories argument in render_template() is variable name passed to 
+    # template. It is assigned to value of categories as defined above
+    return render_template("categories.html", categories=categories)
+
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    if request.method == "POST":
+        # create new row in Category table
+        category = Category()
+        category.category_name = request.form.get("category_name")
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("home"))
+        # GET method
+    return render_template("add_category.html")
+
+@app.route("/edit_categories")
+def edit_categories():
+    categories = list(Category.query.all())
+    return render_template("edit_categories.html", categories=categories)
