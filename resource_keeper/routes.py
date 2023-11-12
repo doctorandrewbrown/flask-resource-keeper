2204cc3bbf248fddaad477b06ed1d1f9d9d15911
andrew brown <dr.andrew.david.brown@gmail.com>
2023-11-12 16:04:32 +0000

    add route for categories

--- resource_keeper/routes.py
+++ resource_keeper/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, request, redirect, url_for
 from resource_keeper import app, db
+from resource_keeper.models import Category, Resource
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("categories.html")
+
