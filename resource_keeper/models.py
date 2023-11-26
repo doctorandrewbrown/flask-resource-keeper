22afa15fcac62b7c1a82f8ef885a22a902cf4250
andrew brown <dr.andrew.david.brown@gmail.com>
2023-11-26 13:35:27 +0000

    implement model class functions for __repr__

--- resource_keeper/models.py
+++ resource_keeper/models.py
@@ -8,8 +8,8 @@ class Category(db.Model):
     resources = db.relationship("Resource", backref="category", cascade="all, delete", lazy=True)
 
     def __repr__(self):
-        # __repr__ to represent itself in the form of a string
-        return self.category_name
+        # __repr__ to represent category in the form of a string
+        return "category name: {0}".format(self.category_name)
 
 
 class Resource(db.Model):
@@ -17,11 +17,10 @@ class Resource(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     resource_name = db.Column(db.String(50), unique=True, nullable=False)
     resource_url = db.Column(db.VARCHAR(100), unique=True, nullable=False)
+    # text data type allows any length for field
     resource_description = db.Column(db.Text, nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
-        # __repr__ to represent itself in the form of a string
-        return "#{0} - Task: {1} | Urgent: {2}".format(
-            self.id, self.task_name, self.is_urgent
-        )
+        # __repr__ to represent resource in the form of a string
+        return "resource name: {0}".format(self.resource_name)
